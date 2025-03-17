6bff7107812d23caf7553b510358e12defe63cca
valorant-dhruv <dbsoni@ncsu.edu>
2025-03-16 20:29:43 -0400

    E2515 student_teams_controller.rb created

---
+++ app/controllers/api/v1/student_teams_controller.rb
@@ -0,0 +1,36 @@
+class Api::V1::StudentTeamsController < ApplicationController
+
+  def index
+
+  end
+
+
+  def show
+    
+  end
+
+  def create
+
+  end
+
+
+  def update
+    
+  end
+
+ 
+  def destroy
+    
+  end
+
+  def remove_participant
+
+  end
+
+  def add_participant
+    
+  end
+
+
+
+end
